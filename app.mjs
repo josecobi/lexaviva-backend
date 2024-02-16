bee0b373ab3466d5aa0936eb70614bd961f316ff
josecobi <58313777+josecobi@users.noreply.github.com>
2024-02-15 20:11:45 -0500

    Server running

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
