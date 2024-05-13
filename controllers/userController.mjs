a600fdebeb488ce5f7ebedbc868483425d0da869
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-12 21:43:40 -0400

    added logic to logout user, get and update user profile

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -64,21 +64,56 @@ const registerUser = asyncHandler(async (req, res) => {
 // The route is GET /api/users/logout
 // the access is public
 const logoutUser = asyncHandler(async(req, res) =>{
-    res.send('logout user');
+    res.cookie('jwt', '', {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // Get user profile
 // The route is GET /api/users/profile
 // the access is private
-const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
-});
+  const getUserProfile = asyncHandler(async (req, res) => {
+    if (req.user) {
+      res.json({
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+  });
+
 
 // Update user profile
 // The route is PUT /api/users/profile
 // the access is private
-const updateUserProfile = asyncHandler(async(req, res) => {
-    res.send('update user profile');
-});
+
+  const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+  
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+  
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+  
+      const updatedUser = await user.save();
+  
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+  });
 
 export {registerUser, authUser, logoutUser, getUserProfile, updateUserProfile};
