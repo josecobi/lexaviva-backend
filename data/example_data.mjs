5046f66c5214d9af915148fe2bdaf1101cf72903
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-14 13:24:04 -0400

    Updated URLs for images on example_data.mjs

--- data/example_data.mjs
+++ data/example_data.mjs
@@ -1,64 +1,85 @@
 const wordsData = [
   {
     word: "comer",
-    imgUrl: "https://www.freepik.com/free-vector/hungry-kid-eating-hamburger-enjoying-unhealthy-food-cartoon-girl-sitting-table-cafeteria-fastfood-restaurant-eat-big-burger-lunch-dinner-isolated-white_28945902.htm#query=to eat&position=24&from_view=search&track=ais&uuid=c2902c4b-5e45-4bae-89bb-18f21870b34b",
+    imgUrl: "https://img.freepik.com/free-vector/hungry-kid-eating-hamburger-enjoying-unhealthy-food-cartoon-girl-sitting-table-cafeteria-fastfood-restaurant-eat-big-burger-lunch-dinner-isolated-white_575670-2362.jpg?t=st=1715686311~exp=1715689911~hmac=bccabaebb907ae2be5da116fab560f766e69554ec09e812b7c3ad9bf940ec30b&w=740",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to eat",
+    user_id: "exampleId"
   },
   {
     word: "jugar",
-    imgUrl: "https://www.freepik.com/free-vector/union-happy-school-friends-dance-circle-white-cartoon-children-play-ring-boys-girls-holding-hands-perspective-view-from-top-bottom_28945880.htm#&position=13&from_view=author&uuid=fb3ff243-6105-44e8-9f92-ef3902944fdb",
+    imgUrl: "https://img.freepik.com/free-vector/union-happy-school-friends-dance-circle-white-cartoon-children-play-ring-boys-girls-holding-hands-perspective-view-from-top-bottom_575670-2356.jpg?t=st=1715686342~exp=1715689942~hmac=e14d24df2ab73e229a764ad4ba235541e7a6b88955dc88c4da5dd323a99ec8cc&w=740",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to play",
+    user_id: "exampleId"
   },
   {
     word: "hacer ejercicio",
-    imgUrl: "https://www.freepik.com/free-vector/people-exercising-fitness-gym-room-with-sport-equipment-workouts-woman-men-training-treadmill-bike-lifting-dumbbells-healthy-lifestyle_27668685.htm#page=2&position=43&from_view=author&uuid=fb3ff243-6105-44e8-9f92-ef3902944fdb",
+    imgUrl: "https://img.freepik.com/free-vector/people-exercising-fitness-gym-room-with-sport-equipment-workouts-woman-men-training-treadmill-bike-lifting-dumbbells-healthy-lifestyle_575670-1883.jpg?t=st=1715686426~exp=1715690026~hmac=792daf266bde63baa8e0e69a293927372c270dbbaa026261171782b9ff14c3bc&w=1380",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to exercise",
+    user_id: "exampleId"
   },
   {
     word: "dibujar/pintar",
-    imgUrl: "https://www.freepik.com/free-vector/children-painting-wall-kindergarten-kids-doing-creative-art-with-brushes-little-happy-boys-girl-drawing-tree-bird-squirrel-with-paint-together_24023070.htm#page=3&position=32&from_view=author&uuid=fb3ff243-6105-44e8-9f92-ef3902944fdb",
+    imgUrl: "https://img.freepik.com/free-vector/children-painting-wall-kindergarten-kids-doing-creative-art-with-brushes-little-happy-boys-girl-drawing-tree-bird-squirrel-with-paint-together_575670-638.jpg?t=st=1715686341~exp=1715689941~hmac=959bc29e1c430c6cb7101a7e01dc2209759ab710e32797bd05ba59adc35cd2b7&w=740",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to draw/paint",
+    user_id: "exampleId"
   },
   {
     word: "limpiar",
-    imgUrl: "https://www.freepik.com/free-vector/man-washing-bathtub-sanitary-service-worker-cartoon-character-guy-overalls-holding-sponge-cleaning-bathroom_23591705.htm#page=3&position=38&from_view=author&uuid=fb3ff243-6105-44e8-9f92-ef3902944fdb",
+    imgUrl: "https://img.freepik.com/free-vector/man-washing-bathtub-sanitary-service-worker-cartoon-character-guy-overalls-holding-sponge-cleaning-bathroom_575670-416.jpg?t=st=1715686342~exp=1715689942~hmac=ee7c79e3ebb9b55b4b6f51b075d2d831d438da900cc47a8c96f31a68f16a1a36&w=826",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to clean",
+    user_id: "exampleId"
   },
   {
     word: "bailar",
-    imgUrl: "https://www.freepik.com/free-vector/latin-dance-party-ballroom-club-school-master-class-with-dancing-couple-cartoon-motion-happy-woman-man-dancers-bachata-stylish-lettering-isolated-white_28945960.htm#page=4&position=25&from_view=author&uuid=fb3ff243-6105-44e8-9f92-ef3902944fdb",
+    imgUrl: "https://img.freepik.com/free-vector/latin-dance-party-ballroom-club-school-master-class-with-dancing-couple-cartoon-motion-happy-woman-man-dancers-bachata-stylish-lettering-isolated-white_575670-2378.jpg?t=st=1715686342~exp=1715689942~hmac=8f8f90cd7493db4bdf175c09cbd5cf20d71942b236757a300836c333db15a961&w=740",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to dance",
+    user_id: "exampleId"
   },
   {
     word: "cantar",
-    imgUrl: "https://www.freepik.com/free-vector/woman-karaoke-singer-pop-star-performing-stage-spotlight-young-attractive-girl-showing-expressive-singing-musical-vocalist-participating-talent-show-live-concert_24758637.htm#query=sing&position=4&from_view=author&uuid=6b5232cd-4bfe-49d4-920b-35a1469c7d7f",
+    imgUrl: "https://img.freepik.com/free-vector/woman-karaoke-singer-pop-star-performing-stage-spotlight-young-attractive-girl-showing-expressive-singing-musical-vocalist-participating-talent-show-live-concert_575670-938.jpg?t=st=1715686342~exp=1715689942~hmac=ac7717fd7434675c05dfb38f4ccd205fffc143e4f3d36ffafee22c3ec42becb5&w=826",
     attribution: "Image by studia4rt on Freepik",
     topic: "Basic actions",
+    english_word: "to sing",
+    user_id: "exampleId"
   },
   {
     word: "hablar",
-    imgUrl: "https://www.freepik.com/free-vector/conversation-concept-illustration_7118856.htm#query=conversation&position=2&from_view=search&track=sph&uuid=105a58bb-fa55-4015-a3a1-cec941aca6fc",
+    imgUrl: "https://img.freepik.com/free-vector/conversation-concept-illustration_114360-1102.jpg?t=st=1715686342~exp=1715689942~hmac=e5b7e8d7cbcb2b84f3ec7bfa2dbb62999519066e749fd3daabb62aeb03db1d7e&w=996",
     attribution: "Image by storyset on Freepik",
-        topic: "Basic actions",
+    topic: "Basic actions",
+    english_word: "to talk/speak",
+    user_id: "exampleId"
   },
   {
     word: "correr",
-    imgUrl: "https://www.freepik.com/free-vector/running-concept-illustration_21118097.htm#query=to run&position=2&from_view=search&track=ais&uuid=6297944c-c27d-4e13-9d1c-aa5d7aca8ccb",
+    imgUrl: "https://img.freepik.com/free-vector/running-concept-illustration_114360-8068.jpg?t=st=1715686342~exp=1715689942~hmac=213b0338ca0017fcae411e381682bf66118a74aa344c11f84da75a7e630a2bbf&w=740",
     attribution: "Image by storyset on Freepik",
     topic: "Basic actions",
+    english_word: "to run",
+    user_id: "exampleId"
   },
   {
     word: "dormir",
-    imgUrl: "https://www.freepik.com/free-vector/cute-cat-sleeping-with-pillow-blanket-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_22407133.htm#query=to",
+    imgUrl: "https://img.freepik.com/free-vector/cute-cat-sleeping-with-pillow-blanket-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-4126.jpg?t=st=1715686342~exp=1715689942~hmac=e4c013efef7124b07dad54f820000cd2a048f63989c77b1696005427e697e263&w=740",
     attribution: "Image by catalyststuff on Freepik",
     topic: "Basic actions",
+    english_word: "to sleep",
+    user_id: "exampleId"
   },
 ];
+
 
 export default wordsData;
