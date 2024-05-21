e32767a19afd70f70260c5e0cf1d64ded803dc19
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-21 14:11:46 -0400

    Optimized the function to add sample data.

--- utilities/insert_sample_data.mjs
+++ utilities/insert_sample_data.mjs
@@ -1,43 +1,24 @@
 // Create example data to be inserted in the database
-import wordsData from '../data/example_data.mjs';
+import wordsData from '../data/sample_data.mjs';
 import Word from '../models/wordModel.mjs';
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.mjs';
 
 
 // Insert data into the collection for 'example@example' user
-const insertSampleData = asyncHandler( async () => {
-  const userExample = await User.findOne({email: "example@example.com"});
-  console.log(userExample._id);
+const insertSampleData = asyncHandler( async (user_id) => {
+ 
+  const copyOfWordData = wordsData.reduce((acc, word) => {
+    word.user_id = user_id;
+    acc.push(word)
+    return acc;
+  },[]);
 
-  if (!userExample) {
-    console.log("User not found");
-    return;
-  }
-  wordsData.forEach(async (word) => {
-    word.user_id = userExample._id;
-  });
 
-  await Word.insertMany(wordsData)
+  await Word.insertMany(copyOfWordData)
     console.log('Data inserted successfully');
 });
 
 
-// Copy sample data to the new user's collection and update the user_id
-const copySampleData = asyncHandler(async (user_id) => {
-  const userExample = await User.findOne({email: "example@example.com"});
-  console.log(userExample);
-  const exampleWords = await Word.find({user_id: userExample._id});
-  const newUserSampleWords = exampleWords.map(word => {
-    let wordObj = word.toObject();
-    delete wordObj._id;
-    wordObj.user_id = user_id;
-    return wordObj;
-  });
-  console.log("new user words<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", newUserSampleWords);
-  await Word.insertMany(newUserSampleWords);
-} )
 
-
-
-export { insertSampleData, copySampleData };
+export { insertSampleData };
